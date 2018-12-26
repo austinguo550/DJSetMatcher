ed08776718d5a9904afa37735697b35c31d0b48e
Shivendra Kushwah <shivendrakushwah@gmail.com>
2018-12-25 18:26:11 -0800

    adding distance formula

--- app/Room.py
+++ app/Room.py
@@ -1,3 +1,18 @@
+import Song
+import math
+
 class Room:
 	def __init__(self):
         print("This is the constructor method.")
+
+    def calculate_distance(song1, song2):
+    	features_song1 = song1.features
+    	features_song2 = song2.features
+
+    	distance = 0
+    	for i in range(len(song1)):
+    		distance = distance + (song1[i] - song2[i])**2
+    	return distance ** (1/2.0)
+
+
+
